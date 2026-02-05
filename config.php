f34fe14580753e6b0a80a8778d0410fd703be953
buildwithjaym <jaymmaruji@gmail.com>
2026-02-05 11:43:06 +0800

    chore: update config and documentation

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 
-define('APP_NAME', 'BASC Exam');
-define('MAX_TIME_SECONDS', 20 * 60); // 20 minutes
+define('APP_NAME', 'BSCS QUIZ 1');
+define('MAX_TIME_SECONDS', 20 * 60); // 20 minutes quiz time
 
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'basc_quiz');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_PORT', 3306);
+
